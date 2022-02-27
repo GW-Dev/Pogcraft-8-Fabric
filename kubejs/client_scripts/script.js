2854614dd5dafed22da2bdbbafe35f98e2e21304
GW-Dev <93411305+GW-Dev@users.noreply.github.com>
2022-02-27 17:52:36 -0600

    Among

--- kubejs/client_scripts/script.js
+++ kubejs/client_scripts/script.js
@@ -1,7 +1,55 @@
 // priority: 0
 
 console.info('Hello, World! (You will see this line every time client resources reload)')
+onEvent('ui.main_menu', event => {
+	event.replace(ui => {
+    //ui.background('kubejsui:textures/example_background.png')
+    ui.background('kubejs:textures/e.png')
+    ui.minecraftLogo(30)
+    
+    ui.button(b => {
+      b.name = 'Singleplayer'
+      b.x = (ui.width) / 2 - b.width/2
+      b.y = ((ui.height) / 2) - 30
+      b.action = 'minecraft:singleplayer'
+    })
 
+    ui.button(b => {
+        b.name = 'Multiplayer'
+        b.x = (ui.width) / 2 - b.width/2
+        b.y = (ui.height) / 2
+        b.action = 'minecraft:multiplayer'
+    })
+
+    ui.button(b => {
+      b.name = 'Language'
+      b.x = ui.width - b.width - 10
+      b.y = ui.height - b.height - 10
+      b.action = 'minecraft:language'
+    })
+    
+    ui.button(b => {
+      b.name = 'Accessibility'
+      b.x = 10
+      b.y = ui.height - b.height - 10
+      b.action = 'minecraft:accessibility'
+    })
+
+    ui.button(b => {
+      b.name = 'Options'
+      b.x = (ui.width) / 2 - b.width/2
+      b.y = ((ui.height) / 2) + 30
+      b.action = 'minecraft:options'
+    })
+
+    ui.button(b => {
+      b.name = 'Exit'
+      b.x = (ui.width) / 2 - b.width/2
+      b.y = ((ui.height) / 2) + 60
+      b.action = 'minecraft:quit'
+    })
+  })
+})
 onEvent('jei.hide.items', event => {
 	// Hide items in JEI here
 	// event.hide('minecraft:cobblestone')
--- kubejs/server_scripts/script.js
+++ kubejs/server_scripts/script.js
@@ -17,7 +17,81 @@ onEvent('recipes', event => {
 	  ], {
 		S: 'minecraft:stick',
 		I: '#c:iron_ingots'
-	  })
+	})
+	
+	event.shapeless('2x farmersdelight:raw_pasta', [
+		'4x minecraft:wheat',
+		Item.of('minecraft:potion', {Potion: "minecraft:water"})
+	]).replaceIngredient({item: Item.of('minecraft:potion', {Potion: "minecraft:water"})}, 'minecraft:glass_bottle'),
+	
+	event.custom({
+		"type": "modern_industrialization:macerator",
+		"item_outputs": [
+		Item.of("minecraft:cobblestone", 1)
+		],
+		"duration": 10,
+		"eu": 2,
+		"item_inputs": {
+		 "item" : "minecraft:stone",
+		 "amount": 1
+		}
+	  }).id('p8:1')
+
+	  event.custom({
+		"type": "modern_industrialization:unpacker",
+		"item_outputs": {
+		"item": "minecraft:amethyst_shard",
+		"amount": 4
+		},
+		"duration": 10,
+		"eu": 2,
+		"item_inputs": {
+		 "item" : "minecraft:amethyst_block",
+		 "amount": 1
+		}
+	  }).id('p8:2')
+
+	  event.custom({
+		"type": "modern_industrialization:unpacker",
+		"item_outputs": {
+		"item": "minecraft:quartz",
+		"amount": 4
+		},
+		"duration": 10,
+		"eu": 2,
+		"item_inputs": {
+		 "item" : "minecraft:quartz_block",
+		 "amount": 1
+		}
+	  }).id('p8:3')
+
+	  event.custom({
+		"type": "modern_industrialization:unpacker",
+		"item_outputs": {
+		"item":"spectrum:citrine_shard",
+		"amount": 4
+		},
+		"duration": 10,
+		"eu": 2,
+		"item_inputs": {
+		 "item" : ("spectrum:citrine_block"),
+		 "amount": 1
+		}
+	  }).id('p8:4')
+
+	  event.custom({
+		"type": "modern_industrialization:unpacker",
+		"item_outputs": {
+		"item":"spectrum:topaz_shard",
+		"amount": 4
+		},
+		"duration": 10,
+		"eu": 2,
+		"item_inputs": {
+		 "item" : ("spectrum:topaz_block"),
+		 "amount": 1
+		}
+	  }).id('p8:4')
 })
 
 onEvent('item.tags', event => {
